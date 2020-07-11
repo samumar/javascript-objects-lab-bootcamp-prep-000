56980efd439426c222181170c01c9035f69241ba
samumar43 <samumar43@gmail.com>
2020-07-11 01:58:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value})
 }
 
-updateObjectWithKeyAndValue(obj, prop, 2);
-
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object.key = value;
   return object;
 }
 
-destructivelyUpdateObjectWithKeyAndValue(obj, prop2, 2);
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object, key);
@@ -20,12 +17,8 @@ function deleteFromObjectByKey(object, key) {
   return newObj;
 }
 
-deleteFromObjectByKey(obj, 'prop');
-
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key;
   return object;
 }
-
-destructivelyDeleteFromObjectByKey(obj, 'prop');
